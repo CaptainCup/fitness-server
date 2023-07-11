391ef2575216c04da8b6a13247cb3f7ff182fff7
CaptainCup <poi.lincoln@gmail.com>
2023-07-11 16:27:45 +0800

    sms fix

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -7,6 +7,7 @@ import {
 import { HttpService } from '@nestjs/axios';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { catchError, lastValueFrom } from 'rxjs';
 import { SendSmsDto } from './dto/send-sms.dto';
 import { AcceptPhoneDto } from './dto/accept-phone.dto';
 import { SmsToken } from './schemas/sms.schema';
@@ -44,9 +45,25 @@ export class SmsService {
       .map(([key, val]) => `${key}=${encodeURIComponent(val)}`)
       .join('&');
 
-    const baseUrl = `https://${login}:${api}@gate.smsaero.ru/v2/sms/send?${query}`;
+    const baseUrl = `https://${login}:${api}@gate.smsaero.ru/v2/sms/${
+      process.env.NODE_ENV !== 'production' ? 'testsend' : 'send'
+    }?${query}`;
 
-    await this.httpService.get(baseUrl);
+    const request = await this.httpService
+      .get(baseUrl, {
+        auth: {
+          username: login,
+          password: api,
+        },
+      })
+      .pipe(
+        catchError((error) => {
+          this.logger.error(error.response.data);
+          throw new BadRequestException('Произошла ошибка при отправке кода');
+        }),
+      );
+
+    await lastValueFrom(request);
 
     this.logger.debug(`Send message to ${smsDto.number}`);
   }
